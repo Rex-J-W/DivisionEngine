5e866319cd3c26eeb0ca1d2a8348739e99ac2888
Rex Woodfield <42045447+Rex-J-W@users.noreply.github.com>
2025-09-24 01:20:53 -0400

    Other windows not registering correctly, testing. DataGrid package added

--- DivisionEngine/ViewModels/AssetsWindowViewModel.cs
+++ DivisionEngine/ViewModels/AssetsWindowViewModel.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 
 namespace DivisionEngine.Editor.ViewModels
 {
     public partial class AssetsWindowViewModel : EditorWindowViewModel
     {
+        public ObservableCollection<Debug.LogEntry> Logs { get; } = [];
+
         public AssetsWindowViewModel()
         {
             Title = "Assets";
--- DivisionEngine/ViewModels/EnvironmentWindowViewModel.cs
+++ DivisionEngine/ViewModels/EnvironmentWindowViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DivisionEngine.Editor.ViewModels
+﻿namespace DivisionEngine.Editor.ViewModels
 {
     public partial class EnvironmentWindowViewModel : EditorWindowViewModel
     {
