a84e6ae03f5a80f80c8dad0ade629ff3681f762e
Rex Woodfield <42045447+Rex-J-W@users.noreply.github.com>
2025-09-25 23:02:53 -0400

    Add important classes

---
+++ DivisionEngine.Core/Time.cs
@@ -0,0 +1,22 @@
+﻿using DivisionEngine.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisionEngine
+{
+    /// <summary>
+    /// Manages the timing and frame rate of the engine.
+    /// </summary>
+    public class Time
+    {
+        /// <summary>
+        /// Instance of the Time class, used for singleton access.
+        /// </summary>
+        public static Time? Instance { get; private set; }
+
+
+    }
+}
