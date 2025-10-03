dee9e260062c7d803c9405873a0b4db41b0f8fa1
Rex Woodfield <42045447+Rex-J-W@users.noreply.github.com>
2025-10-03 02:55:08 -0400

    Add psedo random number generator

---
+++ DivisionEngine.Core/Math/Random.cs
@@ -0,0 +1,11 @@
+﻿namespace DivisionEngine.Math
+{
+    /// <summary>
+    /// Represents a pseduo random number generator that holds state.
+    /// </summary>
+    /// <remarks>This class is a placeholder and has not been completed yet</remarks>
+    public class Random
+    {
+
+    }
+}
