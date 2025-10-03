2ce2fd277a96726deb552df13c913418997de055
Rex Woodfield <42045447+Rex-J-W@users.noreply.github.com>
2025-10-03 02:42:14 -0400

    Add light component

---
+++ DivisionEngine.Core/Components/Light.cs
@@ -0,0 +1,10 @@
+﻿namespace DivisionEngine.Components
+{
+    /// <summary>
+    /// Emits light from an entity, affecting the lighting of the world.
+    /// </summary>
+    public class Light : IComponent
+    {
+
+    }
+}
