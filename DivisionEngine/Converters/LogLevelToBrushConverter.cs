2fc3bbbe53fab6d688d3a7240e30445b9ad00aba
Rex Woodfield <42045447+Rex-J-W@users.noreply.github.com>
2025-10-04 20:19:04 -0400

    Build out converters region

---
+++ DivisionEngine/Converters/LogLevelToBrushConverter.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System;
+using System.Globalization;
+
+namespace DivisionEngine.Editor.Converters
+{
+    internal class LogLevelToBrushConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return value switch
+            {
+                null => Brushes.White,
+                LogLevel.Error => Brushes.Red,
+                LogLevel.Warning => Brushes.Yellow,
+                LogLevel.Info => Brushes.White,
+                _ => Brushes.White
+            };
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DivisionEngine/Views/EnvironmentWindow.axaml.cs
+++ DivisionEngine/Views/EnvironmentWindow.axaml.cs
@@ -22,19 +22,31 @@ public partial class EnvironmentWindow : UserControl
         renderWindowUpdate.Start(); // Start the render window tracking update loop
     }
 
+    private void UserControl_Unloaded(object? sender, VisualTreeAttachmentEventArgs e)
+    {
+        renderWindowUpdate?.Stop();
+    }
+
     /// <summary>
     /// Updates the position, size, and visibility of the renderer's window to match the current position and
     /// dimensions of the <see cref="RenderBackground"/> element.
     /// </summary>
     private void UpdateRendererPosition()
     {
-        if (RenderVisualizerFrame == null || App.Renderer?.RendererWindow == null)
-            return;
+        try
+        {
+            if (RenderVisualizerFrame == null || App.Renderer?.RendererWindow == null)
+                return;
 
-        PixelPoint screenPoint = RenderVisualizerFrame.PointToScreen(new Point(0, 0));
-        Size size = RenderVisualizerFrame.Bounds.Size;
+            PixelPoint screenPoint = RenderVisualizerFrame.PointToScreen(new Point(0, 0));
+            Size size = RenderVisualizerFrame.Bounds.Size;
 
-        App.Renderer.RendererWindow!.Position = new Silk.NET.Maths.Vector2D<int>(screenPoint.X, screenPoint.Y);
-        App.Renderer.RendererWindow.Size = new Silk.NET.Maths.Vector2D<int>((int)size.Width, (int)size.Height);
+            App.Renderer.RendererWindow!.Position = new Silk.NET.Maths.Vector2D<int>(screenPoint.X, screenPoint.Y);
+            App.Renderer.RendererWindow.Size = new Silk.NET.Maths.Vector2D<int>((int)size.Width, (int)size.Height);
+        }
+        catch
+        {
+            Debug.Error("Failed to update renderer window position.");
+        }
     }
 }
